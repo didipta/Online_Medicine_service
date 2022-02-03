9cd4b7f62b25512a98fab70d40cbf450af2097c1
didipta <66532706+didipta@users.noreply.github.com>
2022-02-03 17:49:06 +0600

    Initial commit

---
+++ Online_Medicine_service/Controllers/indexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Medicine_service.Controllers
+{
+    public class indexController : Controller
+    {
+        // GET: index
+        public ActionResult Indexpage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Online_Medicine_service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Online_Medicine_service.MvcApplication" Language="C#" %>
